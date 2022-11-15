8f636da67504a32514f13c92fb85a39e825c1d43
Renan Valladão <renancvalladao@gmail.com>
2022-11-15 13:46:18 -0300

    Making splatter generic

--- scripts/paint_area.gd
+++ scripts/paint_area.gd
@@ -1,10 +1,7 @@
 extends Area2D
 
 var Util = preload("res://scripts/util.gd")
-var splatters = {
-	Util.Colors.BLUE: preload("res://scenes/splatter_blue.tscn"),
-	Util.Colors.ORANGE: preload("res://scenes/splatter_orange.tscn")
-}
+var splatter = preload("res://scenes/splatter.tscn")
 
 func _ready():
 	connect("body_entered", self, "_body_entered")
@@ -13,9 +10,9 @@ func _body_entered(other: Node):
 	if (other.has_method("get_color")):
 		var color = other.get_color()
 		if color != Util.Colors.NO_COLOR:
-			var splatter_node = splatters[color].instance()
+			var splatter_node = splatter.instance()
 			splatter_node.position = position
+			splatter_node.set_color(color)
 			var parent = get_parent()
 			parent.call_deferred("add_child", splatter_node)
-			#parent.add_child(splatter_node)
 			queue_free()
--- scripts/splatter.gd
+++ scripts/splatter.gd
@@ -1,7 +1,11 @@
 extends StaticBody2D
 
-const Util = preload("res://scripts/util.gd")
-export(Util.Colors) var color
+var Util = preload("res://scripts/util.gd")
+var sprites = {
+	Util.Colors.BLUE: preload("res://assets/splatter_blue.png"),
+	Util.Colors.ORANGE: preload("res://assets/splatter_orange.png")
+}
 
-func _ready():
+func set_color(color):
+	$Sprite.texture = sprites[color]
 	set_collision_layer_bit(color + 3, true)
