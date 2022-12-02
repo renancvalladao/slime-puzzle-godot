d7c119f73334a2f68f1f561153631d9b84203376
Renan Valladão <renancvalladao@gmail.com>
2022-12-02 11:14:13 -0300

    Adding exceptions to paint area

--- scripts/paint_area_container.gd
+++ scripts/paint_area_container.gd
@@ -5,10 +5,13 @@ var tile_size = Util.tile_size
 var paint_area = preload("res://scenes/paint_area.tscn")
 export(Vector2) var top_left
 export(Vector2) var bottom_right
+export(PoolVector2Array) var exceptions
 
 func _ready():
 	for x in range(top_left.x, bottom_right.x + tile_size, tile_size):
 		for y in range(top_left.y, bottom_right.y + tile_size, tile_size):
+			if (exceptions.has(Vector2(x, y))):
+				continue
 			var paint_area_node = paint_area.instance()
 			paint_area_node.position = Vector2(x, y)
 			add_child(paint_area_node)
