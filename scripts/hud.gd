9fc870b06c3f2cfe1e9792b0e3dba6686666b931
Renan Valladão <renancvalladao@gmail.com>
2022-12-16 00:52:31 -0300

    Adding basic HUD

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
 
 signal reset_game
+signal level_select
 
-func _on_TextureButton_pressed():
+func _on_ResetButton_pressed():
 	emit_signal("reset_game")
+
+func _on_BackButton_pressed():
+	emit_signal("level_select")
--- scripts/level.gd
+++ scripts/level.gd
@@ -2,3 +2,6 @@ extends Node2D
 
 func reset_game():
 	var _error = get_tree().reload_current_scene()
+
+func level_select():
+	var _error = get_tree().change_scene("res://scenes/level_selector.tscn")
