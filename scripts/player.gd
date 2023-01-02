c7a9d93eda954cecefe02cf43981ae78d2621f63
Renan Valladão <renancvalladao@gmail.com>
2023-01-02 15:50:14 -0300

    Fixing cleaner behavior

--- scripts/player.gd
+++ scripts/player.gd
@@ -72,16 +72,17 @@ func _unhandled_input(event: InputEvent) -> void:
 func change_color(new_color) -> void:
 	if color == new_color:
 		return
-	if new_color == Util.Colors.NO_COLOR:
-		for utilColor in Util.Colors:
-			ray_cast.set_collision_mask_bit(Util.Colors[utilColor] + 3, false)
 	if color == Util.Colors.NO_COLOR:
 		for utilColor in Util.Colors:
 			ray_cast.set_collision_mask_bit(Util.Colors[utilColor] + 3, true)
 	sprite.texture = sprites[new_color]
-	ray_cast.set_collision_mask_bit(new_color + 3, false)
-	if color != Util.Colors.NO_COLOR:
-		ray_cast.set_collision_mask_bit(color + 3, true)
+	if new_color == Util.Colors.NO_COLOR:
+		for utilColor in Util.Colors:
+			ray_cast.set_collision_mask_bit(Util.Colors[utilColor] + 3, false)
+	else:
+		ray_cast.set_collision_mask_bit(new_color + 3, false)
+		if color != Util.Colors.NO_COLOR:
+			ray_cast.set_collision_mask_bit(color + 3, true)
 	color = new_color
 
 func get_color():
